9b9e95403cea6ef2546d98eafcd7904f94e0f0d4
Dilip Roshitha <dilip.roshitha@grubtech.com>
2022-11-21 14:41:33 +0530

    CQRS pattern implementation WIP - I

---
+++ customer-service/customer-domain/customer-application-service/src/main/java/com/food/ordering/system/customer/service/domain/CustomerApplicationServiceImpl.java
@@ -0,0 +1,14 @@
+package com.food.ordering.system.customer.service.domain;
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Validated
+@Service
+public class CustomerApplicationServiceImpl {
+
+
+}
